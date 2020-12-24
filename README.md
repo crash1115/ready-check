f90ca23ad501c4a2f317e9e00576021f13eea31d
Chelsea Ash <crash1115@yahoo.com>
2020-12-24 15:05:51 -0500

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# ready-check
+![GitHub All Releases](https://img.shields.io/github/downloads/crash1115/ready-check/total) ![GitHub Releases](https://img.shields.io/github/downloads/crash1115/ready-check/latest/total) ![GitHub release (latest by date)](https://img.shields.io/github/v/release/crash1115/ready-check?label=latest%20version)
+
+# About This Module
+Ready Set Go! is a system agnostic module for Foundry VTT designed to help GM's and players communicate when they're ready to play. GM's are given the option to initiate ready checks, and players can indicate that they're ready (or not) via a button near the chat input.
+
+# GM Instructions
+To initiate a ready check, click on the hourglass button above the chat input. This will reset all player statuses to "Not Ready" and put a pop-up on each player's screen asking them if they're ready. When they click Ready, a green check mark will appear next to their name in the players window.
+
+You can use the Ready Check when you first start up the game, right before combat starts, or when you're taking a quick break so you can see when everyone is back and ready to play!
+
+# Player Instructions
+To change your status from "Ready" to "Not Ready" and vice versa, click on the hourglass button above the chat input. This will display a dialog with buttons you can use to set your desired status. When you set your status to "Ready", a green check mark should appear next to your name. When you're set to "Not Ready", a red X will display instead.
+
+You can use this toggle to help your GM see when you have to step away to take a phone call, answer the door, slay the kobolds that have suddenly invaded your office, or when you just need a couple extra minutes to sort through your 9,000 spells to figure out which ones you'd like to prepare today. Pfft, wizards...
+
+# Compatibility
+- Should be compatible with all systems.
+- Might have issues with other modules that modify the players UI. I've tested it with a few and it seems to work okay, but you never know.
+
+# Installation
+## Using The Manifest URL
+1. Open up Foundry
+2. Navigate to the Add-On Modules tab
+3. Click Install Module
+4. Paste the following URL into the Manifest URL text field: https://raw.githubusercontent.com/crash1115/ready-check/main/module.json
+5. Click Install
+
+# Got Questions?
+Contact me on Discord (CRASH1115#2944).
+
+# License
+- This work is licensed under a [Creative Commons Attribution 4.0 International License](https://creativecommons.org/licenses/by/4.0/legalcode).
+- This work is licensed under the [Foundry Virtual Tabletop EULA - Limited License Agreement for Module Development](https://foundryvtt.com/article/license/).
---
+++ main.js
@@ -0,0 +1,118 @@
+Hooks.on('ready', async function(){
+
+  if(game.user.isGM){
+
+    // Set all players ready to false
+    await setAllToNotReady();
+
+    // Add button for GM
+    let title = game.i18n.localize("READYCHECK.UiCheckButton");
+    const readyCheckBtn = $(`<a class="chat-control-icon ready-check" title="` + title + `"><i class="fas fa-hourglass-half"></i></a>`);
+    jQuery(".chat-control-icon").before(readyCheckBtn);
+
+    // Add event listener
+    jQuery(".ready-check").click(async (event) => {
+      event.preventDefault();
+      let data = {action: 'check'};
+      await setAllToNotReady();
+      game.socket.emit('module.ready-check', data);
+    });
+
+  } else {
+
+    // Add button for players
+    let title = game.i18n.localize("READYCHECK.UiChangeButton");
+    const readyCheckBtn = $(`<a class="chat-control-icon ready-check" title="` + title + `"><i class="fas fa-hourglass-half"></i></a>`);
+    jQuery(".chat-control-icon").before(readyCheckBtn);
+
+    // Add event listener
+    jQuery(".ready-check").click(async (event) => {
+      event.preventDefault();
+      displayReadyDialog(false);  //false here means the GM did not send this request
+    });
+  }
+
+  // Do stuff when the socket is triggered
+  game.socket.on('module.ready-check', async (data) =>{
+
+    // Error handling
+    if(!data){
+      ui.notifications.error(game.i18n.localize("READYCHECK.Error"));
+    }
+
+    // Sending
+    else if(data.action === 'check'){
+      displayReadyDialog(true);  //true here means the GM sent the request
+    }
+
+    // Responding
+    else if (data.action === 'reply'){
+      processReadyResponse(data);
+    }
+
+  });
+
+});
+
+Hooks.on('renderPlayerList', async function(){
+  // Cycle through each player and update the ready status icon in the player window
+  for(var i=0; i < game.users.players.length; i++){
+    let ready = await game.users.players[i].getFlag('ready-check','isReady');
+    let userId = game.users.players[i].data._id;
+    let userName = game.users.players[i].data.name;
+    let indicator;
+
+    // Construct the indicator
+    if(ready){
+      let title = game.i18n.localize("READYCHECK.PlayerReady");
+      indicator = `<i class="fas fa-check ready-indicator ready" title="` + title + `"></i>`;
+    }
+    else {
+      let title = game.i18n.localize("READYCHECK.PlayerNotReady");
+      indicator = `<i class="fas fa-times ready-indicator not-ready" title="` + title + `"></i>`;
+    }
+
+    // Add the indicator to the end of the line
+    $("[data-user-id="+userId+"]").append(indicator);
+  }
+})
+
+// DISPLAY READY CHECK DIALOG AND SEND RESPONSE TO GM (PLAYER)
+function displayReadyDialog(fromGM){
+  let data = {action: 'reply', ready: false, userId: game.user.data._id};
+  let buttons = {yes: {icon: "<i class='fas fa-check'></i>", label: game.i18n.localize("READYCHECK.DialogButtonReady"), callback: () => data.ready = true} };
+  // If the GM didn't initiate this check, give them the option to set ready status to "no"
+  if(!fromGM) {
+    buttons.no = {icon: "<i class='fas fa-times'></i>", label: game.i18n.localize("READYCHECK.DialogButtonNotReady"), callback: () => data.ready = false};
+  }
+  new Dialog({
+    title: game.i18n.localize("READYCHECK.DialogTitle"),
+    content: game.i18n.localize("READYCHECK.DialogContent"),
+    buttons: buttons,
+    default: "yes",
+    close: async () => {
+      game.socket.emit('module.ready-check', data);
+    }
+  }).render(true);
+}
+
+// PROCESS READY CHECK RESPONSE (GM)
+async function processReadyResponse(data){
+  if(game.user.isGM){
+    // Get the appropriate user and set the flag
+    let userToUpdate = game.users.get(data.userId);
+    await userToUpdate.setFlag('ready-check', 'isReady', data.ready);
+    // Update the display in the corner
+    // Hooks.call('renderPlayerList');
+    ui.players.render();
+  }
+}
+
+// SET ALL USERS STATUS TO NOT READY
+async function setAllToNotReady(){
+  if(game.user.isGM){
+    for(var i=0; i < game.users.players.length; i++){
+      await game.users.players[i].setFlag('ready-check','isReady', false);
+    }
+  }
+}
